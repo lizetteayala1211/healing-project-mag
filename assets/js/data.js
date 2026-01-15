8c11c8b17d1c61f2b4153f11364b7e3bfdc90dbd
Lizette Ayala <lizetteayala1211@gmail.com>
2026-01-14 19:49:42 -0600

    Update article folder, pages and JS data

--- assets/js/data.js
+++ assets/js/data.js
@@ -8,7 +8,7 @@ export const navItems = [
 ];
 
 export const cards = [
-  { title: "Finding Purpose in Communities", byline: "By Pamela Smart", color: "#3f5448", path: "/article/finding-purpose" },
+  { title: "Finding Purpose in Communities", byline: "By Pamela Smart", color: "#3f5448", path: "/article/finding-purpose-in-communities" },
   { title: "Untitled", byline: "By Remy Trail", color: "#9a775e", path: "/article/untitled-remy" },
   { title: "Untitled", byline: "By Leonard Wilson", color: "#d2a100", path: "/article/untitled-leonard" },
 
@@ -16,13 +16,13 @@ export const cards = [
     title: "“This is a Place of Vanguards”: A Portrait of the South Bronx",
     byline: "By Liza Austria, Grant Aumell, Nieves Ayress, Vicky Ayress, Melissa Barber, Walter Bosque, Libertad Guerra, Mychal Johnson, and Monxo López",
     color: "#c54a36",
-    path: "/article/vanguards",
+    path: "/article/this-is-a-place-of-vanguards",
     span2: true,
   },
 
   { title: "Magnolia House", byline: "By Meagan Betances", color: "#3c8f92", path: "/article/magnolia-house" },
-  { title: "The Bully and the Bullied", byline: "By Urhiiness Hill", color: "#7faa86", path: "/article/bully" },
-  { title: "The Community I See", byline: "By Aminata Konate", color: "#a9c9d6", path: "/article/community-i-see" },
-  { title: "The Hands That Help", byline: "By Sarah Sanchez", color: "#0f1832", path: "/article/hands-that-help" },
-  { title: "We Deserve Wellness", byline: "By Members of The Liberation Program at Brotherhood Sister Sol", color: "#d8a6a3", path: "/article/wellness" },
+  { title: "The Bully and the Bullied", byline: "By Urhiiness Hill", color: "#7faa86", path: "/article/the-bully-and-the-bullied" },
+  { title: "The Community I See", byline: "By Aminata Konate", color: "#a9c9d6", path: "/article/the-community-i-see" },
+  { title: "The Hands That Help", byline: "By Sarah Sanchez", color: "#0f1832", path: "/article/the-hands-that-help" },
+  { title: "We Deserve Wellness", byline: "By Members of The Liberation Program at Brotherhood Sister Sol", color: "#d8a6a3", path: "/article/we-deserve-wellness" },
 ];
