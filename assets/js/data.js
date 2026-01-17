db4298d2348603da2977fa62add9de70bdab81cc
Lizette Ayala <lizetteayala1211@gmail.com>
2026-01-16 22:26:51 -0600

    Slotting in community board article pages

--- assets/js/data.js
+++ assets/js/data.js
@@ -8,22 +8,27 @@ export const navItems = [
 ];
 
 export const cards = [
-  { title: "Finding Purpose in Communities", byline: "By Pamela Smart", color: "#3f5448", path: "/article/finding-purpose-in-communities" },
-  { title: "Untitled", byline: "By Remy Trail", color: "#9a775e", path: "/article/untitled-remy" },
-  { title: "Untitled", byline: "By Leonard Wilson", color: "#d2a100", path: "/article/untitled-leonard" },
+  { title: "Finding Purpose in Communities", byline: "by Pamela Smart", color: "#3f5448", path: "/article/finding-purpose-in-communities" },
+  { title: "Untitled", byline: "by Remy Trail", color: "#9a775e", path: "/article/untitled-remy" },
+  { title: "Untitled", byline: "by Leonard Wilson", color: "#d2a100", path: "/article/untitled-leonard" },
 
   {
     title: "“This is a Place of Vanguards”: A Portrait of the South Bronx",
-    byline: "By Liza Austria, Grant Aumell, Nieves Ayress, Vicky Ayress, Melissa Barber, Walter Bosque, Libertad Guerra, Mychal Johnson, and Monxo López",
+    byline: "by Liza Austria, Grant Aumell, Nieves Ayress, Vicky Ayress, Melissa Barber, Walter Bosque, Libertad Guerra, Mychal Johnson, and Monxo López",
     color: "#c54a36",
     path: "/article/this-is-a-place-of-vanguards",
   },
 
-  { title: "Magnolia House", byline: "By Meagan Betances", color: "#3c8f92", path: "/article/magnolia-house" },
+  { title: "Magnolia House", byline: "by Meagan Betances", color: "#3c8f92", path: "/article/magnolia-house" },
   { title: "The Bully and the Bullied", byline: "By Urhiiness Hill", color: "#7faa86", path: "/article/the-bully-and-the-bullied" },
-  { title: "The Community I See", byline: "By Aminata Konate", color: "#a9c9d6", path: "/article/the-community-i-see" },
-  { title: "The Hands That Help", byline: "By Sarah Sanchez", color: "#0f1832", path: "/article/the-hands-that-help" },
-  { title: "We Deserve Wellness", byline: "By Members of The Liberation Program at Brotherhood Sister Sol", color: "#d8a6a3", path: "/article/we-deserve-wellness" },
+  { title: "The Community I See", byline: "by Aminata Konate", color: "#a9c9d6", path: "/article/the-community-i-see" },
+  { title: "The Hands That Help", byline: "by Sarah Sanchez", color: "#0f1832", path: "/article/the-hands-that-help" },
+  { title: "We Deserve Wellness", byline: "by Members of The Liberation Program at Brotherhood Sister Sol", color: "#d8a6a3", path: "/article/we-deserve-wellness" },
+
+  { title: "A Call to Join Forces with Incarcerated Artists", byline: "by Parole Prep", color: "#3f5448", path: "/article/a-call-to-join" },
+  { title: "A Call to Breathe Land Back to Life", byline: "by South Bronx Unite/The Land Stewards", color: "#9a775e", path: "/article/a-call-to-breathe" },
+  { title: "A Call To Language & Practitioners", byline: "by The Fortune Society", color: "#d2a100", path: "/article/a-call-to-language" },
+  { title: "A Call to Save Our Schools", byline: "by Brotherhood Sister Sol", color: "#c54a36", path: "/article/a-call-to-save" },
 ];
 
 export const sectionFeatured = {
